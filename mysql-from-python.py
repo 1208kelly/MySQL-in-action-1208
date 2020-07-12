05146a0bf994fe4baff47b48229996481d428e28
1208kelly <1208kelly@gmail.com>
2020-07-12 20:01:56 +0000

    Updating cursor

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,22 +1,23 @@
 import os
+import datetime
 import pymysql
 
-#Get username from workspace
+# Get username from workspace
 username = os.getenv('gh_user')
 
-#Connect to the database
-connection = pymysql.connect(host = 'localhost',
+# Connect to the database
+connection = pymysql.connect(host='localhost',
                             user=username,
                             password='',
                             db='Chinook')
 
 try:
-    #Run a query
-    with connection.cursor() as cursor:
-        sql = "select * from Artist;"
+    # Run a query
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = "select * from Genre;"
         cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        for row in cursor:
+            print(row)
 finally:
-    #Close the connection, regardless of whether the above was successful
+    # Close the connection, regardless of whether the above was successful
     connection.close()
